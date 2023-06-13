1afa089b4c189f47ba223c02912b9a66a0b5487e
jaemini_man <58785929+jmlee8939@users.noreply.github.com>
2023-06-13 15:04:11 +0900

    Initial Commit

---
+++ dramatic rehab/dramatic_rehabApp.swift
@@ -0,0 +1,17 @@
+//
+//  dramatic_rehabApp.swift
+//  dramatic rehab
+//
+//  Created by Jaemin Lee on 2023/06/13.
+//
+
+import SwiftUI
+
+@main
+struct dramatic_rehabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
